ce80f276246ddcc9638b6601888e26965add4fdb
Liam McCartney <liam.mccartney@gmail.com>
2022-12-23 13:36:30 -0500

    Set up formatting shortcut

--- .config/nvim/lua/core/plugin_config/lsp_config.lua
+++ .config/nvim/lua/core/plugin_config/lsp_config.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
- ensure_installed = { "sumneko_lua" }
+	ensure_installed = { "sumneko_lua" }
 })
 
 local on_attach = function(_, _)
@@ -10,6 +10,7 @@ local on_attach = function(_, _)
 	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
 	vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
 	vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+	vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, {})
 end
 
 require("lspconfig").sumneko_lua.setup {
