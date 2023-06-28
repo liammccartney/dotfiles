03871684dcd8d425d52e9d3df40c6c0245ba8105
Liam McCartney <liam.mccartney@gmail.com>
2023-06-28 15:18:03 -0400

    WIP

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,8 @@
 require("neodev").setup()
-require("core.options")
-require("core.keymaps")
+require("options")
+require("keymaps")
+require("plugins")
+
 --
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
@@ -21,28 +23,40 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup({
-  {'folke/tokyonight.nvim'},
+  { 'folke/tokyonight.nvim' },
   {
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     dependencies = {
       -- LSP Support
-      {'neovim/nvim-lspconfig'}, 
+      { 'neovim/nvim-lspconfig' },
       {
         'williamboman/mason.nvim',
         build = function()
-          pcall(vim.cmd, 'MasonUpdate')
+          pcall(function() vim.cmd('MasonUpdate') end)
         end,
       },
-      {'williamboman/mason-lspconfig.nvim'},
+      { 'williamboman/mason-lspconfig.nvim' },
 
       -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     
-      {'hrsh7th/cmp-nvim-lsp'}, 
-      {'L3MON4D3/LuaSnip'},    
+      { 'hrsh7th/nvim-cmp' },
+      { 'hrsh7th/cmp-nvim-lsp' },
+      { 'L3MON4D3/LuaSnip' },
     }
   },
-  { "folke/neodev.nvim", opts = {} }
+  { "folke/neodev.nvim",              opts = {} },
+  { 'nvim-treesitter/nvim-treesitter' },
+  { 'nvim-telescope/telescope.nvim',  tag = '0.1.1', dependencies = { 'nvim-lua/plenary.nvim' } },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  }
 })
 
 vim.opt.termguicolors = true
@@ -50,9 +64,44 @@ vim.cmd.colorscheme('tokyonight')
 
 local lsp = require('lsp-zero').preset({})
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-end)
+lsp.ensure_installed({
+  'tsserver',
+  'eslint',
+  'angularls',
+  'html',
+  'jsonls',
+  'omnisharp'
+})
+
+local on_attach = function(_, bufnr)
+  lsp.default_keymaps({ buffer = bufnr })
+
+  vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, { buffer = true })
+end
+
+lsp.on_attach(on_attach)
+
+lsp.configure('angularls', {
+  root_dir = require('lspconfig').util.root_pattern("package.json", "tsconfig.base.json"),
+})
+
+lsp.configure('omnisharp', {
+  root_dir = require('lspconfig').util.find_git_ancestor,
+  on_attach = function (client, bufnr)
+    -- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1492605642
+    local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
+    for i, v in ipairs(tokenModifiers) do
+      local tmp = string.gsub(v, ' ', '_')
+      tokenModifiers[i] = string.gsub(tmp, '-_', '')
+    end
+    local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
+    for i, v in ipairs(tokenTypes) do
+      local tmp = string.gsub(v, ' ', '_')
+      tokenTypes[i] = string.gsub(tmp, '-_', '')
+    end
+    on_attach(client, bufnr)
+  end,
+})
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
