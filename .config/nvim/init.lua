9c276ada0edc39b9fc56bece969ea78c32b3a97b
Liam McCartney <liam.mccartney@gmail.com>
2023-06-28 14:25:35 -0400

    Initial state for lsp-zero
    Followed the Tutorial to create a new init.lua file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,59 @@
-require('impatient');
-require("core.options");
-require("core.keymaps");
-require("core.plugins");
-require("core.plugin_config");
+require("neodev").setup()
+require("core.options")
+require("core.keymaps")
+--
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+
+-- auto install lazy.nvim if not present
+if not vim.loop.fs_stat(lazypath) then
+  print('Installing lazy.nvim')
+  vim.fn.system({
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable',
+    lazypath,
+  })
+  print('Done')
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  {'folke/tokyonight.nvim'},
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      -- LSP Support
+      {'neovim/nvim-lspconfig'}, 
+      {
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+      {'williamboman/mason-lspconfig.nvim'},
+
+      -- Autocompletion
+      {'hrsh7th/nvim-cmp'},     
+      {'hrsh7th/cmp-nvim-lsp'}, 
+      {'L3MON4D3/LuaSnip'},    
+    }
+  },
+  { "folke/neodev.nvim", opts = {} }
+})
+
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('tokyonight')
+
+local lsp = require('lsp-zero').preset({})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
