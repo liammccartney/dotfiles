828f410df735e3a7f09279189df1cff2350dfeff
Liam McCartney <liam.mccartney@gmail.com>
2023-12-10 21:35:07 -0500

    remove omnisharp tokens bug workaround, it'd fixed now

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -29,20 +29,6 @@ lsp.configure('angularls', {
 
 lsp.configure('omnisharp', {
   root_dir = require('lspconfig').util.find_git_ancestor,
-  on_attach = function(client, bufnr)
-    -- https://github.com/OmniSharp/omnisharp-roslyn/issues/2483#issuecomment-1492605642
-    local tokenModifiers = client.server_capabilities.semanticTokensProvider.legend.tokenModifiers
-    for i, v in ipairs(tokenModifiers) do
-      local tmp = string.gsub(v, ' ', '_')
-      tokenModifiers[i] = string.gsub(tmp, '-_', '')
-    end
-    local tokenTypes = client.server_capabilities.semanticTokensProvider.legend.tokenTypes
-    for i, v in ipairs(tokenTypes) do
-      local tmp = string.gsub(v, ' ', '_')
-      tokenTypes[i] = string.gsub(tmp, '-_', '')
-    end
-    on_attach(client, bufnr)
-  end,
 })
 
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
