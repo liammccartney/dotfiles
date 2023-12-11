8938c1a3b600c734d1ada104a80ee0f92685b78d
Liam McCartney <liam.mccartney@gmail.com>
2023-12-10 22:05:30 -0500

    Add duskfox

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "EdenEast/nightfox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("duskfox")
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "duskfox",
+    },
+  },
+}
