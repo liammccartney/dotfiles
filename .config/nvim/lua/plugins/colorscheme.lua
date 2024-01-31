5c2cc872c791a61c4aaa0dd17e31ba4e2cfc8ce2
Liam McCartney <liam.mccartney@gmail.com>
2024-01-31 10:51:42 -0500

    [Colorscheme] Make comments more visible
    would like to find a way to make this toggleable

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,14 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+      require('catppuccin').setup({
+        custom_highlights = function(colors)
+          return {
+            Comment = { fg = colors.flamingo }
+          }
+        end
+      })
+
       vim.cmd.colorscheme "catppuccin"
     end
   }
