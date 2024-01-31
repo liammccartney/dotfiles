52977de29f15e7f84d9aada94366ea7e3f0667f8
Liam McCartney <liam.mccartney@gmail.com>
2024-01-31 11:47:06 -0500

    [Git] Set up Gitsigns Keys, move fugitive

--- .config/nvim/lua/plugins/tpope.lua
+++ .config/nvim/lua/plugins/tpope.lua
@@ -1,8 +1,4 @@
 return {
-  -- TODO: Probably another plugin that might be worth checking out
-  -- for git integration
-  { 'tpope/vim-fugitive' },
   { 'tpope/vim-abolish' },
-  -- TODO: Experiment with mini.surround instead
   { 'tpope/vim-surround' },
 }
