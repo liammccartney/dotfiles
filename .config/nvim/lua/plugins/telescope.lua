3b73b1119e1b4f461e0326fc11af5f1f62bf6e52
Liam McCartney <liam.mccartney@gmail.com>
2024-01-31 10:56:14 -0500

    [Telescope] Not Lazy!

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
   version = false,
+  lazy = false,
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-ui-select.nvim' }
