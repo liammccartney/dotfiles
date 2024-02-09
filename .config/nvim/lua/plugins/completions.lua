20546a5cfd275497dd56d8688caf1d0251384e41
Liam McCartney <liam.mccartney@gmail.com>
2024-02-09 17:05:50 -0500

    Add lspkind for nice symbols

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,87 +1,98 @@
 -- TODO: Refactor to a separate module
 local function get_cmp()
-	local ok_cmp, cmp = pcall(require, "cmp")
-	return ok_cmp and cmp or {}
+  local ok_cmp, cmp = pcall(require, "cmp")
+  return ok_cmp and cmp or {}
 end
 
 local function get_luasnip()
-	local ok_luasnip, luasnip = pcall(require, "luasnip")
-	return ok_luasnip and luasnip or {}
+  local ok_luasnip, luasnip = pcall(require, "luasnip")
+  return ok_luasnip and luasnip or {}
 end
 
 local function luasnip_supertab(select_opts)
-	local cmp = get_cmp()
-	local luasnip = get_luasnip()
+  local cmp = get_cmp()
+  local luasnip = get_luasnip()
 
-	return cmp.mapping(function(fallback)
-		local col = vim.fn.col(".") - 1
+  return cmp.mapping(function(fallback)
+    local col = vim.fn.col(".") - 1
 
-		if cmp.visible() then
-			cmp.select_next_item(select_opts)
-		elseif luasnip.expand_or_jumpable() then
-			luasnip.expand_or_jump()
-		elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
-			fallback()
-		else
-			cmp.complete()
-		end
-	end, { "i", "s" })
+    if cmp.visible() then
+      cmp.select_next_item(select_opts)
+    elseif luasnip.expand_or_jumpable() then
+      luasnip.expand_or_jump()
+    elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+      fallback()
+    else
+      cmp.complete()
+    end
+  end, { "i", "s" })
 end
 
 local function luasnip_shift_supertab(select_opts)
-	local cmp = get_cmp()
-	local luasnip = get_luasnip()
+  local cmp = get_cmp()
+  local luasnip = get_luasnip()
 
-	return cmp.mapping(function(fallback)
-		if cmp.visible() then
-			cmp.select_prev_item(select_opts)
-		elseif luasnip.jumpable(-1) then
-			luasnip.jump(-1)
-		else
-			fallback()
-		end
-	end, { "i", "s" })
+  return cmp.mapping(function(fallback)
+    if cmp.visible() then
+      cmp.select_prev_item(select_opts)
+    elseif luasnip.jumpable(-1) then
+      luasnip.jump(-1)
+    else
+      fallback()
+    end
+  end, { "i", "s" })
 end
 
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
+  {
+    "hrsh7th/cmp-nvim-lsp",
+  },
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "onsails/lspkind.nvim" },
+    },
+    config = function()
+      local cmp = require("cmp")
+      require("luasnip.loaders.from_vscode").lazy_load()
 
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<Tab>"] = luasnip_supertab(),
-					["<S-Tab>"] = luasnip_shift_supertab(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" },
-				}, {
-					{ name = "buffer" },
-				}),
-			})
-		end,
-	},
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+          end,
+        },
+        window = {
+          completion = cmp.config.window.bordered(),
+          documentation = cmp.config.window.bordered(),
+        },
+        mapping = cmp.mapping.preset.insert({
+          ["<Tab>"] = luasnip_supertab(),
+          ["<S-Tab>"] = luasnip_shift_supertab(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        formatting = {
+          fields = { "abbr", "kind", "menu" },
+          format = require("lspkind").cmp_format({
+            mode = "symbol",
+            maxwidth = 50,
+            ellipsis_char = "...",
+          }),
+        },
+        sources = cmp.config.sources({
+          { name = "nvim_lsp" },
+          { name = "luasnip" },
+        }, {
+          { name = "buffer" },
+        }),
+      })
+    end,
+  },
 }
