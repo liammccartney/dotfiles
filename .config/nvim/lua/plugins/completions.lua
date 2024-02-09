af9d060a9824895712fb06b737fd18e969bd3060
Liam McCartney <liam.mccartney@gmail.com>
2024-02-09 17:03:29 -0500

    Refactor completions to use the supertab i like

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -1,81 +1,87 @@
+-- TODO: Refactor to a separate module
+local function get_cmp()
+	local ok_cmp, cmp = pcall(require, "cmp")
+	return ok_cmp and cmp or {}
+end
+
+local function get_luasnip()
+	local ok_luasnip, luasnip = pcall(require, "luasnip")
+	return ok_luasnip and luasnip or {}
+end
+
+local function luasnip_supertab(select_opts)
+	local cmp = get_cmp()
+	local luasnip = get_luasnip()
+
+	return cmp.mapping(function(fallback)
+		local col = vim.fn.col(".") - 1
+
+		if cmp.visible() then
+			cmp.select_next_item(select_opts)
+		elseif luasnip.expand_or_jumpable() then
+			luasnip.expand_or_jump()
+		elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+			fallback()
+		else
+			cmp.complete()
+		end
+	end, { "i", "s" })
+end
+
+local function luasnip_shift_supertab(select_opts)
+	local cmp = get_cmp()
+	local luasnip = get_luasnip()
+
+	return cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item(select_opts)
+		elseif luasnip.jumpable(-1) then
+			luasnip.jump(-1)
+		else
+			fallback()
+		end
+	end, { "i", "s" })
+end
+
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp",
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    config = function()
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-            and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        -- TODO: Refactor to use lsp-zero's style of supertab which i think is subtly different
-        -- and i got really used to over the past few months [2024-02-09]
-        -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/lua/lsp-zero/cmp-mapping.lua
-        mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif has_words_before() then
-              cmp.complete()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ['<CR>'] = cmp.mapping({
-            i = function(fallback)
-              if cmp.visible() and cmp.get_active_entry() then
-                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
-              else
-                fallback()
-              end
-            end,
-            s = cmp.mapping.confirm({ select = true }),
-            c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
-          })
-        },
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-        }, {
-          { name = "buffer" },
-        }),
-      })
-    end,
-  },
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<Tab>"] = luasnip_supertab(),
+					["<S-Tab>"] = luasnip_shift_supertab(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
+	},
 }
