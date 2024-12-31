48b025d7710b1eff66e5e2031dbc0187fe5872db
Liam McCartney <liam.mccartney@gmail.com>
2024-12-30 22:48:02 -0500

    Bring in my classic key maps
    format some files
    map leader to comma cause that's for me
    configure telescop
    try to create multigrep telescope picker

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,17 +2,57 @@
 -- https://github.com/neovim/neovim/issues/31675
 vim.hl = vim.highlight
 
-require("config.lazy")
+local g = vim.g
 
-vim.opt.clipboard = "unnamedplus"
+g.mapleader = ","
+
+local function map(m, k, v, opts)
+  opts = opts or {}
+  opts["silent"] = true
+  vim.keymap.set(m, k, v, opts)
+end
+
+-- Move between splits with <c-hjkl>
+map("n", "<c-j>", "<c-w>j")
+map("n", "<c-k>", "<c-w>k")
+map("n", "<c-h>", "<c-w>h")
+map("n", "<c-l>", "<c-w>l")
+
+-- Snap search results to middle of page
+map("n", "n", "nzz")
+map("n", "N", "Nzz")
+
+-- Save files
+map("n", "<leader>w", ":up<CR>", { desc = "Save Current File" })
+map("n", "<leader>W", ":wa!<CR>", { desc = "Save All Files" })
+
+-- Toggle between buffers
+map("n", "<leader><leader>", "<c-^>", { desc = "Toggle Between Buffers" })
+
+-- Split vertically quickly
+map("n", "<leader>vs", ":vsplit<CR>", { desc = "Open Vertical Split" })
+
+-- Clear search highlights
+map("n", "<leader><CR>", ":noh<CR>", { desc = "Clear Search Highlights" })
+
+-- Close all open location lists, or w/e they're called
+-- that get opened by other plugins
+map("n", "<leader>cc", ":cclose<cr> :pclose<cr> :lclose<cr>", { desc = "Close all loc and quickfix lists" })
+
+-- Jump list Navigation
+map("n", "<UP>", "<C-o>")
+map("n", "<Down>", "<C-i>")
+map("n", "<Left>", ":bprev<CR>")
+map("n", "<Right>", ":bnext<CR>")
+
 vim.opt.shiftwidth = 2
 vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Keystrokes to re-source a lua file or just execute some lines
-vim.keymap.set("n", "<space><space>x", "<CMD>source %<CR>")
-vim.keymap.set("n", "<space>x", ":.lua<CR>")
-vim.keymap.set("v", "<space>x", ":lua<CR>")
+map("n", "<space><space>x", "<CMD>source %<CR>")
+map("n", "<space>x", ":.lua<CR>")
+map("v", "<space>x", ":lua<CR>")
 
 -- Highlight when yanking text
 vim.api.nvim_create_autocmd('TextYankPost', {
@@ -22,3 +62,5 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+require("config.lazy")
