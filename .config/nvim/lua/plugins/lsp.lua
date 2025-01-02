34febfb64b321bd6cd061e2d21dc2fd7ace06b1f
Liam McCartney <liam.mccartney@gmail.com>
2025-01-02 15:36:35 -0500

    WIP

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,7 +14,68 @@ return {
     },
   },
   config = function()
-    require("lspconfig").lua_ls.setup {}
+    local capabilities = require('blink.cmp').get_lsp_capabilities()
+
+    require("lspconfig").lua_ls.setup { capabilities = capabilities }
+
+    local path_to_omnisharp = vim.fs.joinpath(vim.uv.os_homedir(), "omnisharp/OmniSharp.dll")
+
+    require 'lspconfig'.omnisharp.setup {
+      cmd = { "dotnet", path_to_omnisharp },
+      capabilities = capabilities,
+      settings = {
+        FormattingOptions = {
+          -- Enables support for reading code style, naming convention and analyzer
+          -- settings from .editorconfig.
+          -- EnableEditorConfigSupport = true,
+          -- Specifies whether 'using' directives should be grouped and sorted during
+          -- document formatting.
+          OrganizeImports = nil,
+        },
+        MsBuild = {
+          -- If true, MSBuild project system will only load projects for files that
+          -- were opened in the editor. This setting is useful for big C# codebases
+          -- and allows for faster initialization of code navigation features only
+          -- for projects that are relevant to code that is being edited. With this
+          -- setting enabled OmniSharp may load fewer projects and may thus display
+          -- incomplete reference lists for symbols.
+          LoadProjectsOnDemand = nil,
+        },
+        RoslynExtensionsOptions = {
+          -- Enables support for roslyn analyzers, code fixes and rulesets.
+          EnableAnalyzersSupport = nil,
+          -- Enables support for showing unimported types and unimported extension
+          -- methods in completion lists. When committed, the appropriate using
+          -- directive will be added at the top of the current file. This option can
+          -- have a negative impact on initial completion responsiveness,
+          -- particularly for the first few completion sessions after opening a
+          -- solution.
+          EnableImportCompletion = nil,
+          -- Only run analyzers against open files when 'enableRoslynAnalyzers' is
+          -- true
+          AnalyzeOpenDocumentsOnly = nil,
+        },
+        Sdk = {
+          -- Specifies whether to include preview versions of the .NET SDK when
+          -- determining which version to use for project loading.
+          IncludePrereleases = true,
+        },
+      },
+    }
+
+    require 'lspconfig'.rust_analyzer.setup {
+      capabilities = capabilities,
+      settings = {
+        ['rust-analyzer'] = {
+          diagnostics = {
+            enable = true,
+          }
+        }
+      }
+    }
+
+    -- require 'lspconfig'.angularls.setup()
+
     vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format() end)
 
     vim.api.nvim_create_autocmd('LspAttach', {
@@ -32,6 +93,34 @@ return {
           })
         end
       end
+    })
+
+    -- TODO: Move to Lazy Keys Prop
+    vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+    vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+
+    -- TODO: Does Lazy Have a Autocmd Prop?
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+        local opts = { buffer = ev.buf }
+
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+
+        vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<F3>", function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
+        vim.keymap.set({ "n", "v" }, "<F4>", vim.lsp.buf.code_action, opts)
+      end,
     })
   end
 }
