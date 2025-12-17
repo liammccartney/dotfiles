9f27e96a731363acd11479040f3afca9b707ea16
Liam McCartney <liam.mccartney@gmail.com>
2025-12-17 12:09:25 -0500

    the quest for the perfect colorscheme continues

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,31 +1,40 @@
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   name = "kanagawa",
+--   lazy = false,
+--   priority = 1000,
+--   config = function()
+--     require("kanagawa").setup({
+--       compile = false,
+--       undercurl = true,
+--       commentStyle = { italic = true },
+--       keywordStyle = { italic = false },
+--       statementStyle = { bold = true },
+--       transparent = false,
+--       dimInactive = false,
+--       terminalColors = true,
+--
+--       colors = {
+--         theme = {
+--           all = {
+--             ui = {
+--               bg_gutter = "none",
+--             },
+--           },
+--         },
+--       },
+--     })
+--
+--     -- pick one:
+--     vim.cmd("colorscheme kanagawa-wave") -- default
+--   end,
+-- }
+
+-- lua/plugins/rose-pine.lua
 return {
-  "rebelot/kanagawa.nvim",
-  name = "kanagawa",
-  lazy = false,
-  priority = 1000,
+  "rose-pine/neovim",
+  name = "rose-pine",
   config = function()
-    require("kanagawa").setup({
-      compile = false,
-      undercurl = true,
-      commentStyle = { italic = true },
-      keywordStyle = { italic = false },
-      statementStyle = { bold = true },
-      transparent = false,
-      dimInactive = false,
-      terminalColors = true,
-
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none",
-            },
-          },
-        },
-      },
-    })
-
-    -- pick one:
-    vim.cmd("colorscheme kanagawa-wave") -- default
-  end,
+    vim.cmd("colorscheme rose-pine-moon")
+  end
 }
